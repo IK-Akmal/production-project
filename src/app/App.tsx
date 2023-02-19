40efc310a4cd207197c44e5e026f8b625d49ef7a
Akmal Isomadinov <isomadinovakmal1997@gmail.com>
2023-02-19 19:36:21 +0500

    Router decomposition

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,5 @@
-import { Suspense } from "react";
-import { Link, Route, Routes } from "react-router-dom";
-
-import { AboutPage } from "pages/AboutPage";
-import { MainPage } from "pages/MainPage";
+import { Link } from "react-router-dom";
+import AppRouter from "./providers/router";
 import { useTheme } from "app/providers/ThemeProvider";
 
 import { classNames } from "shared/lib/classNames";
@@ -17,13 +14,7 @@ const App = () => {
       <button onClick={toggleTheme}>Toggle theme</button>
       <Link to="/">Главная</Link>
       <Link to="/about">О сайте</Link>
-
-      <Suspense fallback={<div>...Loading</div>}>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/about" element={<AboutPage />} />
-        </Routes>
-      </Suspense>
+      <AppRouter />
     </div>
   );
 };
---
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -0,0 +1,24 @@
+import { AboutPage } from "pages/AboutPage";
+import { MainPage } from "pages/MainPage";
+import { RouteProps } from "react-router-dom";
+
+export enum AppRoutes {
+  Main = "main",
+  About = "about",
+}
+
+export const RoutePath: Record<AppRoutes, string> = {
+  [AppRoutes.Main]: "/",
+  [AppRoutes.About]: "/about",
+};
+
+export const routeConfig: Record<AppRoutes, RouteProps> = {
+  [AppRoutes.Main]: {
+    path: RoutePath.main,
+    element: <MainPage />,
+  },
+  [AppRoutes.About]: {
+    path: RoutePath.about,
+    element: <AboutPage />,
+  },
+};
