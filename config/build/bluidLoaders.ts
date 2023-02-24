ed39464232cc9f961e08f669d7e8a403f015fb70
Akmal Isomadinov <isomadinovakmal1997@gmail.com>
2023-02-24 22:37:47 +0500

    Added svgr and file loader

--- config/build/bluidLoaders.ts
+++ config/build/bluidLoaders.ts
@@ -1,35 +1,46 @@
-import { BuildOptions } from './types/config';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import webpack from 'webpack';
+import { BuildOptions } from "./types/config";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import webpack from "webpack";
 
 export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+  const svgLoader: webpack.RuleSetRule = {
+    test: /\.svg$/,
+    use: ["@svgr/webpack"],
+  };
 
-    const typescriptLoader: webpack.RuleSetRule = {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: "/node_modules/",
-    };
+  const typescriptLoader: webpack.RuleSetRule = {
+    test: /\.tsx?$/,
+    use: "ts-loader",
+    exclude: "/node_modules/",
+  };
 
-    const cssLoader: webpack.RuleSetRule = {
-        test: /\.s[ac]ss$/i,
-        use: [
-            isDev ? "style-loader" : MiniCssExtractPlugin.loader,
-            {
-                loader: "css-loader",
-                options: {
-                    modules: {
-                        auto: (resPath: string) => resPath.includes('.module.'),
-                        localIdentName: isDev ? '[path][name]__[local]--[hash:base64:5]' : "[hash:base64:8]"
-                    },
-                }
-            },
-            "sass-loader",
-        ],
-    };
+  const cssLoader: webpack.RuleSetRule = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+      {
+        loader: "css-loader",
+        options: {
+          modules: {
+            auto: (resPath: string) => resPath.includes(".module."),
+            localIdentName: isDev
+              ? "[path][name]__[local]--[hash:base64:5]"
+              : "[hash:base64:8]",
+          },
+        },
+      },
+      "sass-loader",
+    ],
+  };
 
-    return [
-        typescriptLoader,
-        cssLoader
-    ]
+  const fileLoader: webpack.RuleSetRule = {
+    test: /\.(png|jpe?g|gif|woff2?)$/i,
+    use: [
+      {
+        loader: "file-loader",
+      },
+    ],
+  };
 
+  return [fileLoader, svgLoader, typescriptLoader, cssLoader];
 }
