95b6dc7672d0debcc08faddebb99358c881a7264
Akmal Isomadinov <isomadinovakmal1997@gmail.com>
2024-01-08 20:36:13 +0500

    Added react-hooks rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,7 @@ module.exports = {
         ecmaVersion: 'latest',
         sourceType: 'module',
     },
-    plugins: ['react', '@typescript-eslint', 'i18next'],
+    plugins: ['react', '@typescript-eslint', 'i18next', 'react-hooks'],
     rules: {
         indent: [2, 4],
 
@@ -69,6 +69,9 @@ module.exports = {
 
         'no-shadow': 'off',
         '@typescript-eslint/no-shadow': 'error',
+
+        'react-hooks/rules-of-hooks': 'error',
+        'react-hooks/exhaustive-deps': 'error',
     },
     globals: {
         isDev: true,
